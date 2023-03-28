1b0255838cbcd26a384fa2b656b509b0534cd56f
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-03-28 18:16:45 -0500

    Deleted the image because it is not necessary

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
-
+  
   if (isLoading) {
     return <div>Loading ...</div>;
   }
@@ -10,8 +10,7 @@ const Profile = () => {
   return (
     isAuthenticated && (
       <div>
-        <img src={user.picture} alt={user.given_name} />
-        <h2>{user.name}</h2>
+        <h2>Hello, {user.given_name}</h2>
       </div>
     )
   );
