06a06cee557058339c1469dd7bd5eefed6d1de5f
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-03-28 18:17:38 -0500

    Added a button to show and hide the logout option

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import LoginButton from './Login';
 import LogoutButton from './Logout';
 import Profile from './Profile';
 import { useAuth0 } from "@auth0/auth0-react";
-
+import { useState } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,42 +12,53 @@ import {
   Link
 } from 'react-router-dom';
 
-const Layout = ({children}) => {
+const Layout = () => {
   const { isAuthenticated } = useAuth0()
+  const [ showButton, setShowButton ] = useState('hidden')
+
+  const showLogOut = () => {
+    setShowButton(showButton === 'hidden' ? 'block' : 'hidden')
+  }
 
   return (
     <>
       <Router>
-        <div className="App">
-          <ul className="App-header">
-            <li>
-              <Link to="./components/home">Home</Link>
-            </li>
-            <li>
-              <Link to="./components/products">Products</Link>
-            </li>
-            {
-              isAuthenticated ?
-                <div>
+        <div className="App w-3/4 m-auto p-4">
+          <nav className='h-10 leading-10'>
+            <ul className="App-header flex items-center justify-around">
+              <li>
+                <Link to="./components/home">Home</Link>
+              </li>
+              <li>
+                <Link to="./components/products">Products</Link>
+              </li>
+              {
+                isAuthenticated ?
+                  <div>
+                    <li className='flex items-center'>
+                      <Profile />
+                      <button className='flex' onClick={showLogOut}>
+                        <span class="material-symbols-outlined">
+                          keyboard_arrow_down
+                        </span>
+                      </button>
+                    </li>
+                    <p className={showButton}>
+                      <LogoutButton />
+                    </p>
+                  </div> :
                   <li>
-                    <Profile />
+                    <LoginButton />
                   </li>
-                  <li>
-                    <LogoutButton />
-                  </li>
-                </div> :
-                <li>
-                  <LoginButton />
-                </li>
-            }
-          </ul>
+              }
+            </ul>
+          </nav>
           <Routes>
             <Route path='/components/home/*' element={ <Home /> }></Route>
             <Route path='/components/products/*' element={ <Products /> }></Route>
           </Routes>
         </div>
       </Router>
-      {children}
     </>
   )
 }
