21c6bdbfb4e8c70d7485fb7d9b9d9539169e94ae
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-03-28 18:17:19 -0500

    Change in the style of the products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -15,23 +15,24 @@ const Products = () => {
   const wareHouse = inventory.map(product => (
     <div key={product.id}>
       <h2>{product.name}</h2>
-      <p>{product.shortDesc}</p>
     </div>
   ))
 
 	return (
-    <div>
-      <Link to='./shoes'>{wareHouse[0]}</Link>
-      <Link to='./bags'>{wareHouse[1]}</Link>
-      <Link to='./shirts'>{wareHouse[2]}</Link>
-      <Link to='./ties'>{wareHouse[3]}</Link>
+    <>
+      <div className='grid grid-cols-4 place-items-center'>
+        <Link to='./shoes'>{wareHouse[0]}</Link>
+        <Link to='./bags'>{wareHouse[1]}</Link>
+        <Link to='./shirts'>{wareHouse[2]}</Link>
+        <Link to='./ties'>{wareHouse[3]}</Link>
+      </div>
       <Routes>
         <Route path='/shoes/*' element={ <Shoes /> }></Route>
         <Route path='/bags/*' element={ < Bags /> }></Route>
         <Route path='/shirts/*' element={ < Shirts /> }></Route>
         <Route path='/ties/*' element={ < Ties /> }></Route>
       </Routes>
-    </div>
+    </>
   )
 }
 
