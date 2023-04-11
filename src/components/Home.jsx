1ce416bad521f1934f7f5da69743730134da8790
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-04-11 09:52:58 -0500

    Change in the text of main page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
     <section className="my-4">
       <h2 className="text-3xl font-bold pb-4">My commerce</h2>
       <p className="text-2xl pb-2">Here you can find what you're looking for!!!</p>
-      <p className="text-xl">We have everything for you, shoes, bags, shirts, shorts and more!!!</p>
+      <p className="text-xl">We have everything for you, shoes, bags, shirts and more!!!</p>
     </section>
   )
 }
